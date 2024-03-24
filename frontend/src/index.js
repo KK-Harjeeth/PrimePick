39ae8656e72ff555594f26d1ab3a8b2d1ada4e91
KK-Harjeeth <sunny.harjeeth@gmail.com>
2024-03-24 11:30:10 +0530

    commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -12,6 +12,9 @@ import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PrivateRoute from './components/PrivateRoute';
+import PaymentScreen from './screens/PaymentScreen';
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path="/"  element={<App />}>
@@ -20,7 +23,12 @@ const router=createBrowserRouter(
       <Route path="/cart" element={<CartScreen/>}/> 
       <Route path="/login" element={<LoginScreen/>}/> 
       <Route path="/register" element={<RegisterScreen/>}/> 
+      <Route path='' element={<PrivateRoute/>}>
+          <Route path="/shipping" element={<ShippingScreen/>}/> 
+          <Route path="/payment" element={<PaymentScreen/>}/> 
+      </Route>
     </Route>
+
 )
 )
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
-const initialState = localStorage.getItem('cart')?JSON.parse(localStorage.getItem("cart")):{cartItems:[]};
+const initialState = localStorage.getItem('cart')
+                    ?JSON.parse(localStorage.getItem("cart"))
+                    :{cartItems:[],shippingAddress:{},paymentMethod:"PayPal"};
 
 const cartSlice = createSlice({
     name:'cart',
@@ -22,8 +24,16 @@ const cartSlice = createSlice({
         removeFromCart:(state,action)=>{
             state.cartItems=state.cartItems.filter((x)=>x._id!==action.payload);
             return updateCart(state)
+        },
+        saveShippingAddress:(state,action)=>{
+            state.shippingAddress=action.payload;
+            return updateCart(state);
+        },
+        savePaymentMethod:(state,action)=>{
+            state.paymentMethod=action.payload;
+            return updateCart(state);
         }
     },
 })
-export const {addToCart,removeFromCart} = cartSlice.actions;
+export const {addToCart,removeFromCart,saveShippingAddress,savePaymentMethod} = cartSlice.actions;
 export default cartSlice.reducer;

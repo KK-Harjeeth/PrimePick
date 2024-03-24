9f530d37b265ef7ae8e4c86bed1f5ae946d1b7e2
KK-Harjeeth <sunny.harjeeth@gmail.com>
2024-03-24 15:17:41 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import productRoutes from './routes/ProductRoutes.js'
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from "./routes/OrderRoutes.js";
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 const port = process.env.PORT;
 connectDB();
@@ -21,6 +22,7 @@ app.get('/',(req,res)=>{
 
 app.use('/api/products',productRoutes);
 app.use('/api/users',userRoutes);
+app.use('/api/orders',orderRoutes);
 app.use(notFound);
 app.use(errorHandler);
 app.listen(port,()=>console.log( `Server started on ${port}`));  
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,6 +15,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PrivateRoute from './components/PrivateRoute';
 import PaymentScreen from './screens/PaymentScreen';
+import  PlaceOrderScreen from './screens/PlaceOrderScreen';
 const router=createBrowserRouter(
   createRoutesFromElements(
     <Route path="/"  element={<App />}>
@@ -26,6 +27,7 @@ const router=createBrowserRouter(
       <Route path='' element={<PrivateRoute/>}>
           <Route path="/shipping" element={<ShippingScreen/>}/> 
           <Route path="/payment" element={<PaymentScreen/>}/> 
+          <Route path="/placeorder" element={<PlaceOrderScreen/>}/> 
       </Route>
     </Route>
 

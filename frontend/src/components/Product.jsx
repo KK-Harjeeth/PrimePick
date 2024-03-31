ae17a6ef7e2d71813143aa83b8a0a242e9362425
KK-Harjeeth <sunny.harjeeth@gmail.com>
2024-03-31 15:39:25 +0530

    made entire card clickable

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -7,32 +7,30 @@ const Product = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Card
-      className='my-3 p-3 rounded'
-      style={{ transform: isHovered ? 'scale(1.05)' : 'scale(1)' }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      <Link to={`/product/${product._id}`}>
+    <Link to={`/product/${product._id}`} style={{ textDecoration: 'none' }}>
+      <Card
+        className='my-3 p-3 rounded'
+        style={{ transform: isHovered ? 'scale(1.05)' : 'scale(1)' }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <Card.Img src={product.image} variant='top' />
-      </Link>
 
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
+        <Card.Body>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </Link>
 
-        <Card.Text as='div'>
-          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-        </Card.Text>
+          <Card.Text as='div'>
+            <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+          </Card.Text>
 
-        <Card.Text as="h3">
-          ${product.price}
-        </Card.Text>
-      </Card.Body>
-    </Card>
+          <Card.Text as="h3">
+            ${product.price}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 }
 

f16fb0be6ec4c776af1e5e4bd103e2b1467d46bf
KK-Harjeeth <sunny.harjeeth@gmail.com>
2024-03-31 15:03:27 +0530

    product cards zoom on mouse hover

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,43 +1,39 @@
-
-import React from 'react'
-import {Card} from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
-const Product = ({product}) => {
+
+const Product = ({ product }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Card className='my-3 p-3 rounded'>
+    <Card
+      className='my-3 p-3 rounded'
+      style={{ transform: isHovered ? 'scale(1.05)' : 'scale(1)' }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
 
+      <Card.Body>
         <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top'/>
-        </Link> 
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
 
-        {/* body */}
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+        </Card.Text>
 
-        <Card.Body>
-
-            <Link to={`/product/${product._id}`}></Link> 
-
-            {/* Title */}
-
-            <Card.Title as="div">
-                <strong>{product.name}</strong>
-            </Card.Title>
-
-            <Card.Text as='div'>
-                <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
-            </Card.Text>
-
-
-            {/* text */}
-
-            <Card.Text as="h3">
-                ${product.price}
-            </Card.Text>
-
-        </Card.Body>
-
+        <Card.Text as="h3">
+          ${product.price}
+        </Card.Text>
+      </Card.Body>
     </Card>
-  )
+  );
 }
 
-export default Product
+export default Product;

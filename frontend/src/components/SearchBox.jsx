2887868fbeab47480093530f8aec8143e72bc2e8
KK-Harjeeth <sunny.harjeeth@gmail.com>
2024-03-31 15:33:05 +0530

    search bar

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { Form,Button } from 'react-bootstrap'
 import { useParams,useNavigate } from 'react-router-dom'
+import {Col} from 'react-bootstrap'
 const SearchBox = () => {
     const navigate=useNavigate();
     const {keyword:urlKeyword}=useParams();
@@ -9,7 +10,6 @@ const SearchBox = () => {
     const submitHandler=(e)=>{
         e.preventDefault();
         if(keyword.trim()){
-            setKeyword('');
             navigate(`/search/${keyword}`);
         }else{
             navigate('/'); 
@@ -17,11 +17,20 @@ const SearchBox = () => {
     }
 
   return (
-    <Form onSubmit={submitHandler} className='d-flex'>
-        <Form.Control type='text' name='q' onChange={(e)=>setKeyword(e.target.value)} value={keyword} placeholder='Search Products....' className='mr-sm-2 ml-sm-5'></Form.Control>
+    <Form onSubmit={submitHandler} className='d-flex justify-content-center'>
+        {/* <Form.Control type='text' name='q' onChange={(e)=>setKeyword(e.target.value)} value={keyword} placeholder='Search Products....' className='mr-sm-2 ml-sm-5' style={{ width: '900px' }}></Form.Control> */}
+        <Col xs={12} sm={8} md={6} lg={4} xl={3} className="mr-sm-2 ml-sm-5" style={{ width: '900px' }}>
+                <Form.Control
+                    type='text'
+                    name='q'
+                    onChange={(e) => setKeyword(e.target.value)}
+                    value={keyword}
+                    placeholder='Search Products....'
+                />
+            </Col>
         <Button type='submit' variant='outline-light' className='p-2 mx-2'>Search</Button>
     </Form>
   )
 }
 
-export default SearchBox
+export default SearchBox;
